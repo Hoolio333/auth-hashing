41a38ff70ccca90796f84ac6a6904808f830dda3
Chris Hyde <c_hyde333@hotmail.com>
2022-06-15 17:40:07 +0100

    Registration & Login - Completed

--- src/routers/sessions.js
+++ src/routers/sessions.js
@@ -1,26 +1,35 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const prisma = require('../utils/prisma.js')
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const prisma = require("../utils/prisma.js");
 
-router.post('/', async (req, res) => {
-    const { username, password } = req.body;
+router.post("/", async (req, res) => {
+  const { username, password } = req.body;
 
-    const foundUser = await prisma.user.findFirst({
-        where: {
-            username
-        }
-    });
+  const foundUser = await prisma.user.findFirst({
+    where: {
+      username,
+    },
+  });
 
-    if (!foundUser || password !== foundUser.password) {
-        return res.status(401).json({ error: 'Invalid username or password.' });
-    }
+  if (!foundUser) {
+    return res.status(401).json({ error: "Invalid username or password." });
+  }
 
-    const token = jwt.sign({ username }, process.env.JWT_SECRET);
+  const passwordIsValid = await bcrypt.compare(password, foundUser.password);
+  console.log(password);
+  console.log(foundUser.password);
+  console.log(passwordIsValid);
 
-    res.json({ data: token });
+  if (!passwordIsValid) {
+    return res.status(401).json({ error: "Invalid username or password." });
+  }
+
+  const token = jwt.sign({ username }, process.env.JWT_SECRET);
+
+  res.json({ data: token });
 });
 
 module.exports = router;
